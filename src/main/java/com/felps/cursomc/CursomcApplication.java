930e485b305a112ba92768bdd859b7c64c71ac63
felipdsanta <felipe.carro3@outlook.com>
2021-01-11 20:38:54 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/felps/cursomc/CursomcApplication.java
+++ src/main/java/com/felps/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.felps.cursomc.domain.Cidade;
 import com.felps.cursomc.domain.Cliente;
 import com.felps.cursomc.domain.Endereco;
 import com.felps.cursomc.domain.Estado;
+import com.felps.cursomc.domain.ItemPedido;
 import com.felps.cursomc.domain.Pagamento;
 import com.felps.cursomc.domain.PagamentoComBoleto;
 import com.felps.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.felps.cursomc.repositories.CidadeRepository;
 import com.felps.cursomc.repositories.ClienteRepository;
 import com.felps.cursomc.repositories.EnderecoRepository;
 import com.felps.cursomc.repositories.EstadoRepository;
+import com.felps.cursomc.repositories.ItemPedidoRepository;
 import com.felps.cursomc.repositories.PagamentoRepository;
 import com.felps.cursomc.repositories.PedidoRepository;
 import com.felps.cursomc.repositories.ProdutoRepository;
@@ -55,7 +57,10 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -119,6 +124,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 1, 2000.00);
+		ItemPedido ip3 = new ItemPedido(ped1, p2, 100.00 , 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
 	}
 
--- src/main/java/com/felps/cursomc/domain/Pedido.java
+++ src/main/java/com/felps/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.felps.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,24 +12,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
-	@Temporal(TemporalType.TIMESTAMP)
 	private Date instante;
 	
-	@OneToOne(cascade = CascadeType.ALL, mappedBy="pedido")
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
-	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
@@ -35,7 +32,10 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
-	
+
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	public Pedido() {
 	}
 
@@ -46,49 +46,43 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public Date getInstante() {
 		return instante;
 	}
-
 	public void setInstante(Date instante) {
 		this.instante = instante;
 	}
-
 	public Pagamento getPagamento() {
 		return pagamento;
 	}
-
 	public void setPagamento(Pagamento pagamento) {
 		this.pagamento = pagamento;
 	}
-
 	public Cliente getCliente() {
 		return cliente;
 	}
-
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-
 	public Endereco getEnderecoDeEntrega() {
 		return enderecoDeEntrega;
 	}
-
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
@@ -98,7 +92,6 @@ public class Pedido implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -115,7 +108,7 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
+
 
 }
