4d345ec2371f9fe4978e5744882e08636ec8d312
felipdsanta <felipe.carro3@outlook.com>
2021-01-11 17:05:29 -0300

    Endpoints /clientes/{id} disponivel

---
+++ src/main/java/com/felps/cursomc/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.felps.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felps.cursomc.domain.Cliente;
+import com.felps.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService cliente;
+	
+	
+	@RequestMapping(value= "/{id}" , method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = cliente.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
