de2c374f2a4824cab6b56a99f59afe9131b1e05e
felipdsanta <felipe.carro3@outlook.com>
2021-01-11 10:48:53 -0300

    Projeto criado

---
+++ src/test/java/com/felps/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felps.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
