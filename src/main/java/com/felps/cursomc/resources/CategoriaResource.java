9bdf731f1a1c0aefa0500757d6bc5ba748f159e6
felipdsanta <felipe.carro3@outlook.com>
2021-01-11 11:17:56 -0300

    Testando classe dominio - Categoria

--- src/main/java/com/felps/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felps/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.felps.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.felps.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value= "/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "REST está funcionando!";
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+			
+			List<Categoria> lista = new ArrayList<>();
+			lista.add(cat1);
+			lista.add(cat2);
+		
+		return lista;
 	}
 }
