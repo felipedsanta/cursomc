25f550b457bd1cdd0d40a39f954aab091991c6b9
felipdsanta <felipe.carro3@outlook.com>
2021-01-11 21:36:37 -0300

    Inserindo uma nova categoria com POST

--- src/main/java/com/felps/cursomc/domain/Cliente.java
+++ src/main/java/com/felps/cursomc/domain/Cliente.java
@@ -15,7 +15,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.felps.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -30,7 +29,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
