ff715ba776576474ca647c537e493c800a8d1a09
felipdsanta <felipe.carro3@outlook.com>
2021-01-11 21:00:11 -0300

    Endpoint /pedidos/{id} Disponivel

--- src/main/java/com/felps/cursomc/domain/Pedido.java
+++ src/main/java/com/felps/cursomc/domain/Pedido.java
@@ -15,16 +15,22 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern= "dd/MM/yyyy HH:mm")
 	private Date instante;
 	
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
+	
+	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
