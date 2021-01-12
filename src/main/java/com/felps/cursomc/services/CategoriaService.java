f53ce619cf49182976376601a07fde2da893d344
felipdsanta <felipe.carro3@outlook.com>
2021-01-12 15:07:59 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/felps/cursomc/services/CategoriaService.java
+++ src/main/java/com/felps/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.felps.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();	
+		}
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
