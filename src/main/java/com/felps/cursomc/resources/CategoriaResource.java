48bc80bdb9375ac956229c151d8f209ec740631f
felipdsanta <felipe.carro3@outlook.com>
2021-01-13 22:03:07 -0300

    PUT, DELETE E GET PARA CLIENTE

--- src/main/java/com/felps/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felps/cursomc/resources/CategoriaResource.java
@@ -66,10 +66,10 @@ public class CategoriaResource {
 	/* direction ASC or DESC*/
 	@RequestMapping(value="/page", method=RequestMethod.GET)
 	public ResponseEntity<Page<CategoriaDTO>> findPage(
-											@RequestParam(value= "page",defaultValue="0") Integer page, 
-											@RequestParam(value= "linesPerPage",defaultValue="24")Integer linesPerPage, 
-											@RequestParam(value= "orderBy",defaultValue="nome")String orderBy, 
-											@RequestParam(value= "direction",defaultValue="ASC")String direction) {
+					@RequestParam(value= "page",defaultValue="0") Integer page, 
+					@RequestParam(value= "linesPerPage",defaultValue="24")Integer linesPerPage, 
+					@RequestParam(value= "orderBy",defaultValue="nome")String orderBy, 
+					@RequestParam(value= "direction",defaultValue="ASC")String direction) {
 		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
