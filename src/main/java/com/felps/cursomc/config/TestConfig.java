db2625aca1b04aac7e301597c0634a233ce995ed
felipdsanta <felipe.carro3@outlook.com>
2021-03-19 22:49:32 -0300

    Criando profile de teste

---
+++ src/main/java/com/felps/cursomc/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.felps.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.felps.cursomc.services.DBservice;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBservice dbService;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+
+}
